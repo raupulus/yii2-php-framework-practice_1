e85e14e94ce0495ba46ac39bd8fe737cf6ed5578
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-19 16:23:17 +0200

    Generos modelos y CRUD

--- db/rec1ev.sql
+++ db/rec1ev.sql
@@ -1,19 +1,19 @@
 DROP TABLE IF EXISTS usuarios CASCADE;
-
 CREATE TABLE usuarios
 (
-    id       bigserial    PRIMARY KEY
-  , nombre   varchar(255) NOT NULL UNIQUE
-  , password char(64)
+    id       BIGSERIAL    PRIMARY KEY
+  , nombre   VARCHAR(255) NOT NULL UNIQUE
+  , password CHAR(64)
 );
 
 DROP TABLE IF EXISTS citas CASCADE;
-
 CREATE TABLE citas
 (
-    id         bigserial PRIMARY KEY
-  , fecha      date      NOT NULL
-  , hora       time      NOT NULL
-  , usuario_id bigint    NOT NULL REFERENCES usuarios (id)
+    id         BIGSERIAL PRIMARY KEY
+  , fecha      DATE      NOT NULL
+  , hora       TIME      NOT NULL
+  , usuario_id BIGINT    NOT NULL REFERENCES usuarios(id)
                          ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
