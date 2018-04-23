42900d98c9d7b414e22accd47cfec08f4fb17ff3
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-04-23 15:32:24 +0200

    Muestro las citas pendientes de forma separada

--- views/citas/index.php
+++ views/citas/index.php
@@ -1,13 +1,15 @@
 <?php
 
+use app\models\Citas;
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\CitasSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Citas';
+$this->title = 'Citas del usuario '.Yii::$app->user->identity->nombre;
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="citas-index">
@@ -19,13 +21,38 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Nueva Cita', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
+    <div>
+        <?php
+        $model = Citas::citaPendiente()->one();
+        ?>
+
+        <?php if ($model): ?>
+        <h3>Cita pendiente</h3>
+
+        <?= DetailView::widget([
+            'model' => $model,
+            'attributes' => [
+                'fecha',
+                'hora',
+            ],
+        ]) ?>
+
+        <?php else: ?>
+        <h3>No tienes citas pendientes</h3>
+        <?php endIf ?>
+
+    </div>
+
+    <div>
+        <h3>Citas del pasado</h3>
+    </div>
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
             'fecha:date',
             'hora:time',
-
-            ['class' => 'yii\grid\ActionColumn'],
+            //['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
 </div>
